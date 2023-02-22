47cd30a177fae10bc6616a141f315466f510da61
Ihor Susoi <ihor.susoi@gmail.com>
2023-02-22 22:40:08 +0200

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let min1;
+        let max1;
+        let guess;
+        let guessBefore;
+    }
 
     setRange(min, max) {
-
+        GuessingGame.min1=min;
+        GuessingGame.max1=max;
+        GuessingGame.guessBefore=0;
     }
 
     guess() {
-
+        GuessingGame.guess = Math.ceil((GuessingGame.min1 + GuessingGame.max1)/2);
+        return GuessingGame.guess;
     }
 
     lower() {
-
+        GuessingGame.max1 = GuessingGame.guess;
+        return;
     }
 
     greater() {
-
+        GuessingGame.min1 = GuessingGame.guess;
+        return;
     }
 }
 
